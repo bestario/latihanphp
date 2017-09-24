93013e7c19fc32b36be24fa306a0c659d9c50213
bestario <dbztario02@gmail.com>
2017-09-24 11:54:21 +0700

    LEVEL 1 - Validating the Form Values

--- form.php
+++ form.php
@@ -1,12 +1,32 @@
 <?php
 if (isset($_POST['submit']))
 {
+	$name = array("bestario", "maria", "johndoe", "jonny", "ethan");
+
+	$maximum = 10;
+	$minimum = 5;
+
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
-	echo $username;
-	echo "<br/>";
-	echo $password;
+	if(strlen($username) <$minimum )
+	{
+		echo "Username harus memiliki panjang 5 atau lebih";
+	}
+
+	if(strlen($username) >$maximum )
+	{
+		echo "Username tidak boleh lebih panjang dari 10";
+	}
+
+	if (!in_array($username, $name))
+	{
+		echo "Maaf, akses ditolak";
+	}
+	else
+	{
+		echo "Selamat datang";
+	}
 }
 
 ?>
