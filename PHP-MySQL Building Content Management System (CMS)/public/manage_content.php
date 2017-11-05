ba986ffed4a11ff41d4e949c34641fa82bd67696
bestario <dbztario02@gmail.com>
2017-11-05 14:42:40 +0700

    Making Page Assests Reusable

---
+++ PHP-MySQL Building Content Management System (CMS)/public/manage_content.php
@@ -0,0 +1,16 @@
+<?php 
+	include("../includes/layouts/header.php");
+?>
+<div id="main">
+	<div id="navigation">
+		&nbsp;
+	</div>
+<div id="page">
+	<h2>Manage Content</h2>
+	
+	</div>
+</div>
+
+<?php
+	include("../includes/layouts/footer.php");
+?>
