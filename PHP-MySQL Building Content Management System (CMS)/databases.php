dfd3d47e7272636f45efda81f2c22463cc4a53ec
bestario <dbztario02@gmail.com>
2017-11-05 12:29:18 +0700

    088 - Working with retrieved data

--- PHP-MySQL Building Content Management System (CMS)/databases.php
+++ PHP-MySQL Building Content Management System (CMS)/databases.php
@@ -31,14 +31,21 @@
 	<title> Database </title>
 	</head>
 	<body>
+		<ul>
 		<?php
 			//3. Use returned data (if any)
-			while($row = mysqli_fetch_row($result)) {
+			while($subject = mysqli_fetch_assoc($result)) {
 				// output data from each row
-				var_dump($row);
-				echo "";
+		?>
+			<li>
+				<?php 
+					echo $subject["menu_name"]; 
+				?>
+			</li>
+		<?php
 			}
 		?>
+		</ul>
 		<?php
 			//4. Release returned data
 			mysqli_free_result($result);
